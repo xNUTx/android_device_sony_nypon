2d85940559cfb185c7cb3feed362479bbf1c77e6
Percy Gahlot <gahlotpercy@gmail.com>
2014-03-27 14:46:42 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,15 +1,34 @@
 USE_CAMERA_STUB := true
 
-# inherit from the proprietary version
+#
+# Copyright (C) 2014 AndroidOpenSourceXperia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from the proprietary version
 -include vendor/sony/nypon/BoardConfigVendor.mk
 
-#inherit from the common montblanc definitions
+# Inherit from the common montblanc definitions
 -include device/sony/montblanc-common/BoardConfigCommon.mk
 
+# Target device Specific headers
 TARGET_SPECIFIC_HEADER_PATH += device/sony/nypon/include
 
+# Kernel Config
 TARGET_KERNEL_CONFIG := xperiap_defconfig
 
+#HDMI
 COMMON_GLOBAL_CFLAGS += -DSTE_HDMI
 
 # TWRP
@@ -22,13 +41,13 @@ BOARD_VOLD_MAX_PARTITIONS := 16
 
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01400000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x01400000
-BOARD_CMDLINE := device/sony/nypon/config/cmdline.txt
+BOARD_CMDLINE := cachepolicy=writealloc noinitrd init=init board_id=1 root=/dev/ram0 rw rootwait console=ttyAMA2,115200n8 androidboot.console=ttyAMA2 androidboot.hardware=st-ericsson mem=96M@0 mem_mtrace=15M@96M mem_mshared=1M@111M mem_modem=16M@112M mem=32M@128M mem_issw=1M@160M hwmem=167M@161M mem=696M@328M vmalloc=384M mpcore_wdt.mpcore_margin=359
 
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1056964608
 BOARD_USERDATA_PARTITION_SIZE := 2147483648
 BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_FORCE_KERNEL_ADDRESS  := 0x00008000
-BOARD_MKBOOTIMG_ARGS := 0x01000000
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000
 
 BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_USERIMAGES_USE_EXT4 := true
