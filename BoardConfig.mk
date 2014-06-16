e102d9b87a61378b453dcbd2b9a4dbdf89674bd7
percy-g2 <gahlotpercy@gmail.com>
2014-06-17 01:24:45 +0530

    Update for weekly-9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,6 +35,9 @@ BOARD_HAS_NO_REAL_SDCARD := true
 BOARD_HAS_SDCARD_INTERNAL := true
 TARGET_RECOVERY_FSTAB := device/sony/nypon/config/fstab.st-ericsson
 
+# Architecture
+TARGET_ARCH_LOWMEM := true
+
 # Partition information
 BOARD_VOLD_MAX_PARTITIONS := 16
 
